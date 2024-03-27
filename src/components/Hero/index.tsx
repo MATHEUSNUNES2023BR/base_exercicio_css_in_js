9382fccb1746fda6c6fdb469d080533cfc16d37e
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2024-03-27 14:37:29 -0300

    Finalizado Exercício

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { FormHero, HeroTitle } from './style'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <FormHero>
+    <div>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      </HeroTitle>
     </div>
-  </form>
+  </FormHero>
 )
 
 export default Hero
